955c5b10b2c3621a8685e20fad7efb5bee4ced32
krlyeon <sooyeon8427@gmail.com>
2025-05-27 15:56:48 +0900

    Fix:post_list.css 추가

--- project/static/js/post_list.js
+++ project/static/js/post_list.js
@@ -16,3 +16,33 @@ window.onclick = function (event) {
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const boostBtn = document.querySelector(".time-boost-btn_sy button");
+
+    if (boostBtn) {
+        const url = new URL(window.location.href);
+        const isBoosted = url.searchParams.get("burning") === "1";
+
+        // 최초 로딩 시 클래스 반영
+        if (isBoosted) {
+            boostBtn.classList.add("selected_sy");
+        }
+
+        boostBtn.addEventListener("click", function () {
+            const params = new URLSearchParams(window.location.search);
+            const burningFlag = params.get("burning");
+
+            if (burningFlag === "1") {
+                // remove burning flag
+                params.delete("burning");
+            } else {
+                // add burning flag
+                params.set("burning", "1");
+            }
+
+            // 변경된 쿼리 파라미터로 페이지 이동
+            window.location.search = params.toString();
+        });
+    }
+});
