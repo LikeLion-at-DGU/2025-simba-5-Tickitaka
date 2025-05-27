07a11f1191b5c7891331b5ce4ae08f3d2cb15e51
seonwoochoi24 <seonwoo31@dgu.ac.kr>
2025-05-27 17:00:11 +0900

    conflict 해결: posts/urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,10 +5,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('post_list', post_list, name='post_list'),
-<<<<<<< HEAD
-    path('post/<int:id>', post_detail, name='post_detail'),
-    path('post_create/', post_create, name='post_create'),
-=======
     path('post_detail/<int:id>', post_detail, name='post_detail'),
->>>>>>> b3afc6ce76d888db4c2b2eba8755e6dbf4f5eb9c
 ]
