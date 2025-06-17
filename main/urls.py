d63888defd57dac18071f690d069ace5420930eb
krlyeon <sooyeon8427@gmail.com>
2025-06-17 22:28:55 +0900

    Feat: css 수정

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('', mainpage, name='mainpage'),
-    path('home/', home, name = 'home'),
-    path('edit_profile/', edit_profile, name='edit_profile'),
-    path('time_history/', time_history, name='time_history'),
-    path('my_posts/', my_posts, name='my_posts'),
-    path('saved_posts/', saved_posts, name='saved_posts'),
-    # path('friend_list/', friend_list, name='friend_list'),
+    # path('home/', home, name = 'home'),
+    # path('edit_profile/', edit_profile, name='edit_profile'),
+    # path('time_history/', time_history, name='time_history'),
+    # path('my_posts/', my_posts, name='my_posts'),
+    # path('saved_posts/', saved_posts, name='saved_posts'),
+    # # path('friend_list/', friend_list, name='friend_list'),
 ]
