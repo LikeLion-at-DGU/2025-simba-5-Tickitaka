d53af90ae6a35668fa8d7cb8559a0fb3ba06747c
krlyeon <sooyeon8427@gmail.com>
2025-06-18 16:30:06 +0900

    Feat: my_post.html, saved_post.html

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('home/', home, name = 'home'),
     # path('edit_profile/', edit_profile, name='edit_profile'),
     # path('time_history/', time_history, name='time_history'),
-    # path('my_posts/', my_posts, name='my_posts'),
-    # path('saved_posts/', saved_posts, name='saved_posts'),
+    path('my_posts/', my_posts, name='my_posts'),
+    path('saved_posts/', saved_posts, name='saved_posts'),
     # # path('friend_list/', friend_list, name='friend_list'),
 ]
